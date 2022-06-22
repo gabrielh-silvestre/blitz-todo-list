6ceb9424fffd2bc1385a871b0317fb69bfa4720d
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-22 18:56:19 -0300

    feat.test(utils): centralize utility data
    - create mocks and constants for re-use

---
+++ back-end/__tests__/utils/mocks/tasks.mock.ts
@@ -0,0 +1,59 @@
+import { FIRST_USER_ID, SECOND_USER_ID, THIRD_USER_ID } from './users.mock';
+
+const FIRST_MAIN_TASK_ID = 'cl4q28wy400073a82ix9h5bmy';
+const SECOND_MAIN_TASK_ID = 'cl4q299ve00093a82o9pco4fy';
+
+export const tasks = [
+  {
+    id: FIRST_MAIN_TASK_ID,
+    title: 'Go to the store',
+    description: null,
+    userId: FIRST_USER_ID,
+  },
+  {
+    title: 'Buy milk',
+    description: 'Chocolate milk',
+    userId: FIRST_USER_ID,
+    mainTaskId: FIRST_MAIN_TASK_ID,
+  },
+  {
+    title: 'Buy eggs',
+    description: 'Eggs for breakfast',
+    userId: FIRST_USER_ID,
+    mainTaskId: FIRST_MAIN_TASK_ID,
+  },
+  {
+    title: 'Buy bread',
+    description: 'Integral bread',
+    userId: FIRST_USER_ID,
+    mainTaskId: FIRST_MAIN_TASK_ID,
+  },
+  {
+    id: SECOND_MAIN_TASK_ID,
+    title: 'Study',
+    description: 'Learn Elixir',
+    userId: SECOND_USER_ID,
+  },
+  {
+    title: 'Read documentation',
+    description: null,
+    userId: SECOND_USER_ID,
+    mainTaskId: SECOND_MAIN_TASK_ID,
+  },
+  {
+    title: 'Practice in CodeWars',
+    description: 'Solve 3 problems',
+    userId: SECOND_USER_ID,
+    mainTaskId: SECOND_MAIN_TASK_ID,
+  },
+  {
+    title: 'Rest for the exam',
+    description: null,
+    userId: THIRD_USER_ID,
+  },
+];
+
+export const newTask = {
+  title: 'Test task',
+  description: 'Test description',
+};
---
+++ back-end/__tests__/utils/mocks/users.mock.ts
@@ -0,0 +1,30 @@
+export const FIRST_USER_ID = 'cl4q28dge00013a82yht5klxn';
+export const SECOND_USER_ID = 'cl4q28mt300033a82oq6te0u8';
+export const THIRD_USER_ID = 'cl4q28r7000053a82kkqujqsp';
+
+export const users = [
+  {
+    id: FIRST_USER_ID,
+    name: 'John',
+    email: 'john@email.com',
+    password: '123456',
+  },
+  {
+    id: SECOND_USER_ID,
+    name: 'Jane',
+    email: 'jane@email.com',
+    password: '123456',
+  },
+  {
+    id: THIRD_USER_ID,
+    name: 'Jack',
+    email: 'jack@email.com',
+    password: '123456',
+  },
+];
+
+export const newUser = {
+  name: 'Test User',
+  email: 'test@email.com',
+  password: 'test_password',
+};
