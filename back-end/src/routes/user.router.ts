2fa1d9132deae9d85f72969a8521bcebb7757ed5
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-22 23:00:45 -0300

    feat(routes/user): implement endpoint POST /
    - use error middleware on app

--- back-end/src/routes/user.router.ts
+++ back-end/src/routes/user.router.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import { signUpController } from '../modules/users/useCases/signUp';
+
 const UserRouter = Router();
 
 UserRouter.get('/', (_req, res) => {
   res.send('Hello from UserRouter');
 });
+
+UserRouter.post('/', signUpController.handle);
 
 export { UserRouter };
