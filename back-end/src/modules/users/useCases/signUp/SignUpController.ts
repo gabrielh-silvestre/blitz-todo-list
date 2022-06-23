d9d905c5be75baa04c1b991554f80d53da72bf56
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-23 10:15:40 -0300

    feat.test(users/useCases/signUp): cover with unit test signUp case
    - change SignUpController Handler typing

--- back-end/src/modules/users/useCases/signUp/SignUpController.ts
+++ back-end/src/modules/users/useCases/signUp/SignUpController.ts
@@ -1,10 +1,10 @@
-import type { Handler } from 'express';
+import type { Request, Response } from 'express';
 import { SignUpUseCase } from './SignUpUseCase';
 
 class SignUpController {
   constructor(private readonly signUpUseCase: SignUpUseCase) {}
 
-  handle: Handler = async (req, res) => {
+  handle = async (req: Request, res: Response) => {
     const { statusCode, payload } = await this.signUpUseCase.execute(req.body);
 
     res.status(statusCode).json(payload);
---
+++ back-end/src/modules/users/useCases/signUp/__tests__/SignUpController.spec.ts
@@ -0,0 +1,74 @@
+import { NextFunction, response, request } from 'express';
+
+import Sinon from 'sinon';
+import { expect } from 'chai';
+
+import type {
+  SignReturn,
+  SuccessCase,
+  UserCreateAttributes,
+} from '../../../../../@types/types';
+
+import { TokenService } from '../../../../../services/TokenService';
+import { UserRepository } from '../../../repository';
+import { SignUpUseCase } from '../SignUpUseCase';
+import { SignUpController } from '../SignUpController';
+
+const signUpUseCase = new SignUpUseCase(new UserRepository(), TokenService);
+const signUpController = new SignUpController(signUpUseCase);
+
+const FAKE_TOKEN = '0n0v19nASV-V0n09Masvmz0-xasvzx';
+
+const SUCCESS_RESPONSE: SuccessCase<SignReturn> = {
+  statusCode: 201,
+  payload: { token: `Bearer ${FAKE_TOKEN}` },
+};
+
+const CREATE_USER: UserCreateAttributes = {
+  name: 'John Doe',
+  email: 'john@email.com',
+  password: '12345678',
+};
+
+describe('Test SignUpController', () => {
+  let useCaseStub: Sinon.SinonStub;
+  let spiedStatus: Sinon.SinonSpy;
+  let spiedJson: Sinon.SinonSpy;
+  let spiedNext: Sinon.SinonSpy;
+  const next = { next: () => {} } as { next: NextFunction };
+
+  before(() => {
+    spiedStatus = Sinon.spy(response, 'status');
+    spiedJson = Sinon.spy(response, 'json');
+    spiedNext = Sinon.spy(next, 'next');
+  });
+
+  after(() => {
+    spiedStatus.restore();
+    spiedJson.restore();
+    spiedNext.restore();
+  });
+
+  describe('Success case', () => {
+    before(() => {
+      useCaseStub = Sinon.stub(SignUpUseCase.prototype, 'execute');
+      useCaseStub.resolves(SUCCESS_RESPONSE);
+
+      request.body = CREATE_USER;
+    });
+
+    after(() => {
+      useCaseStub.restore();
+    });
+
+    it('should return a success response', async () => {
+      await signUpController.handle(request, response);
+
+      expect(spiedStatus.calledWith(201)).to.be.true;
+
+      expect(spiedJson.args[0][0]).to.be.an('object');
+      expect(spiedJson.args[0][0]).to.have.property('token');
+      expect(spiedJson.args[0][0].token).to.be.an('string');
+    });
+  });
+});
