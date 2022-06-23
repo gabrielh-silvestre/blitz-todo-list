e292ce8872377eda2b792e4dbac3a3e32dfa61ab
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-22 22:59:05 -0300

    feat(middleware): implement generic error middleware
    - normalize error before treatment

---
+++ back-end/src/middleware/ErrorHandler.ts
@@ -0,0 +1,20 @@
+import type { ErrorRequestHandler } from 'express';
+import { HttpError, InternalServerError } from 'restify-errors';
+
+class ErrorHandler {
+  private static readonly normalize = (err: HttpError | Error): HttpError => {
+    if (err instanceof HttpError) {
+      return err;
+    }
+
+    return new InternalServerError(err, 'Internal Server Error');
+  };
+
+  static handler: ErrorRequestHandler = (err, _req, res, _next) => {
+    const error = ErrorHandler.normalize(err);
+
+    res.status(error.statusCode).json({ message: error.message });
+  };
+}
+
+export { ErrorHandler };
