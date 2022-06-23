99e8ee5d6f2700c236ce9238de7679a2d00fb0c7
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-22 22:57:58 -0300

    feat(config): configure jwt options and secret

---
+++ back-end/src/config/jwt.config.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { Secret, SignOptions } from 'jsonwebtoken';
+
+const secret = process.env.JWT_SECRET as Secret;
+
+const jwtOptions: SignOptions = {
+  expiresIn: '3d',
+  algorithm: 'HS256',
+};
+
+export { secret, jwtOptions };
