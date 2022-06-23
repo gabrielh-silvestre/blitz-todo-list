432e2fa718aeba11bba1ef61999c873e7dd45b3c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-23 17:54:00 -0300

    feat(types/user): create type for user authentication

--- back-end/src/@types/types/user.types.ts
+++ back-end/src/@types/types/user.types.ts
@@ -2,8 +2,15 @@ import type { User } from '@prisma/client';
 
 type UserAttributes = User;
 type UserCreateAttributes = Pick<UserAttributes, 'name' | 'email' | 'password'>;
+type UserAuthAttributes = Pick<UserAttributes, 'email' | 'password'>;
 type UserIdentifier = Pick<UserAttributes, 'id'>;
 
 type SignReturn = { token: string };
 
-export { UserAttributes, UserCreateAttributes, UserIdentifier, SignReturn };
+export {
+  UserAttributes,
+  UserCreateAttributes,
+  UserAuthAttributes,
+  UserIdentifier,
+  SignReturn,
+};
