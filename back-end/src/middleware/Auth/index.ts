6593dfa14ae4e9630ac8c7289f3cbb65f32d4fb7
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-25 15:03:01 -0300

    feat(middleware): create middleware for authentication
    - save token data on userId on Request
    - implement endpoint GET /tasks

---
+++ back-end/src/middleware/Auth/index.ts
@@ -0,0 +1,26 @@
+import { Handler } from 'express';
+import { UnauthorizedError } from 'restify-errors';
+
+import type { ITokenService } from '../../@types/interfaces';
+
+import { TokenService } from '../../services/TokenService';
+
+class AuthHandler {
+  constructor(private readonly tokenService: ITokenService) {}
+
+  handle: Handler = (req, _res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) throw new UnauthorizedError('Unauthorized');
+
+    const userId = this.tokenService.verifyToken(token);
+
+    if (!userId) throw new UnauthorizedError('Unauthorized');
+
+    req.userId = userId;
+
+    next();
+  };
+}
+
+export const AuthMiddleware = new AuthHandler(TokenService);
--- back-end/src/routes/task.router.ts
+++ back-end/src/routes/task.router.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
+import { AuthMiddleware } from '../middleware/Auth';
+
+import { findAllController } from '../modules/tasks/useCases/findAll';
+
 const TaskRouter = Router();
 
-TaskRouter.get('/', (_req, res) => {
-  res.send('Hello from TaskRouter');
-});
+// TaskRouter.get('/', (_req, res) => {
+//   res.send('Hello from TaskRouter');
+// });
+
+TaskRouter.get('/', AuthMiddleware.handle, findAllController.handle);
 
 export { TaskRouter };
