163a57343510b38b851ee4135d72304c86de40f0
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-25 19:43:24 -0300

    fix.test(integration/Task/GetAll): add request for token before get tasks
    - change invalid token message

--- back-end/__tests__/integration/Task/GetAll.spec.ts
+++ back-end/__tests__/integration/Task/GetAll.spec.ts
@@ -16,25 +16,32 @@ describe('Test endpoint GET /tasks', () => {
   });
 
   it('Successfully get all tasks', async () => {
-    const response = await request(app)
-      .get(ENDPOINTS.TASK)
-      .auth(email, password);
+    await request(app)
+      .post(ENDPOINTS.LOGIN)
+      .send({ email, password })
+      .then(async (res) => {
+        const { token } = res.body;
 
-    expect(response.status).to.be.equal(200);
+        const response = await request(app)
+          .get(ENDPOINTS.TASK)
+          .set('Authorization', token);
 
-    expect(response.body).to.be.an('array');
-    expect(response.body[0]).to.have.property('id');
-    expect(response.body[0]).to.have.property('title');
-    expect(response.body[0]).to.have.property('description');
-    expect(response.body[0]).to.have.property('status');
-    expect(response.body[0]).to.have.property('subTasks');
-    expect(response.body[0]).to.have.property('createdAt');
-    expect(response.body[0]).to.have.property('lastUpdate');
-    expect(response.body[0]).to.have.property('completedAt');
+        expect(response.status).to.be.equal(200);
 
-    expect(response.body[0]).not.to.have.property('userId');
+        expect(response.body).to.be.an('array');
+        expect(response.body[0]).to.have.property('id');
+        expect(response.body[0]).to.have.property('title');
+        expect(response.body[0]).to.have.property('description');
+        expect(response.body[0]).to.have.property('status');
+        expect(response.body[0]).to.have.property('subTasks');
+        expect(response.body[0]).to.have.property('createdAt');
+        expect(response.body[0]).to.have.property('lastUpdate');
+        expect(response.body[0]).to.have.property('completedAt');
 
-    expect(response.body[0].subTasks).to.be.an('array');
+        expect(response.body[0]).not.to.have.property('userId');
+
+        expect(response.body[0].subTasks).to.be.an('array');
+      });
   });
 
   it('Fail to get all tasks without token', async () => {
@@ -48,10 +55,10 @@ describe('Test endpoint GET /tasks', () => {
   it('Fail to get all tasks with invalid token', async () => {
     const response = await request(app)
       .get(ENDPOINTS.TASK)
-      .auth(email, 'invalid');
+      .set('Authorization', 'invalid token');
 
     expect(response.status).to.be.equal(401);
     expect(response.body).to.have.property('message');
-    expect(response.body.message).to.be.equal('Invalid token');
+    expect(response.body.message).to.be.equal('Expired or invalid token');
   });
 });
