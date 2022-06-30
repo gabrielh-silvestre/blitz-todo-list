601c3a7cf1bef7b10a89a91925e3c6ee23c9cca8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-30 16:04:52 -0300

    feat.test(modules/task/useCases): cover create new task case

---
+++ back-end/src/modules/tasks/useCases/create/__tests__/CreateUseCase.spec.ts
@@ -0,0 +1,92 @@
+import Sinon from "sinon";
+import { expect } from "chai";
+
+import type {
+  TaskReturn,
+  TaskCreateAttributes,
+} from "../../../../../@types/types";
+
+import { TaskRepository } from "../../../repository";
+import { CreateUseCase } from "../CreateUseCase";
+
+const MOCK_NEW_TASK: TaskCreateAttributes = {
+  title: "Task 1",
+  description: "Task 1 description",
+  mainTaskId: "1",
+};
+
+const MOCK_NEW_TASK_RETURN: TaskReturn = {
+  ...MOCK_NEW_TASK,
+  id: "1",
+  status: "DELETED",
+  createdAt: new Date(),
+  lastUpdate: new Date(),
+  completedAt: new Date(),
+};
+
+const createUseCase = new CreateUseCase(new TaskRepository());
+
+describe("Test CreateUseCase", () => {
+  let findByTitleStub: Sinon.SinonStub;
+  let createStub: Sinon.SinonStub;
+
+  describe("Success case", () => {
+    before(() => {
+      findByTitleStub = Sinon.stub(TaskRepository.prototype, "findByTitle");
+      findByTitleStub.resolves(null);
+
+      createStub = Sinon.stub(TaskRepository.prototype, "create");
+      createStub.resolves(MOCK_NEW_TASK_RETURN);
+    });
+
+    after(() => {
+      findByTitleStub.restore();
+      createStub.restore();
+    });
+
+    it("should return a success response with created task", async () => {
+      const response = await createUseCase.execute({ id: "1" }, MOCK_NEW_TASK);
+
+      expect(response).to.be.an("object");
+      expect(response).to.have.property("statusCode");
+      expect(response).to.have.property("payload");
+
+      expect(response.statusCode).to.be.equal(201);
+      expect(response.payload).to.be.an("object");
+
+      expect(response.payload).to.have.property("id");
+      expect(response.payload).to.have.property("title");
+      expect(response.payload).to.have.property("description");
+      expect(response.payload).to.have.property("status");
+      expect(response.payload).to.have.property("mainTaskId");
+      expect(response.payload).to.have.property("createdAt");
+      expect(response.payload).to.have.property("lastUpdate");
+      expect(response.payload).to.have.property("completedAt");
+    });
+  });
+
+  describe("Fail case", () => {
+    before(() => {
+      findByTitleStub = Sinon.stub(TaskRepository.prototype, "findByTitle");
+      findByTitleStub.resolves(MOCK_NEW_TASK_RETURN);
+
+      createStub = Sinon.stub(TaskRepository.prototype, "create");
+      createStub.rejects(new Error("Should not reach this point"));
+    });
+
+    after(() => {
+      findByTitleStub.restore();
+      createStub.restore();
+    });
+
+    it("should throw an error with message and status code", async () => {
+      try {
+        await createUseCase.execute({ id: "1" }, MOCK_NEW_TASK);
+      } catch (error) {
+        expect(error).to.be.an("object");
+        expect(error).to.have.property("statusCode", 409);
+        expect(error).to.have.property("message", "Task already exists");
+      }
+    });
+  });
+});
