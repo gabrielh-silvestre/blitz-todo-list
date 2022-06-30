9dfd3c5fce9026e0b551ee0759fd338da39a3ea0
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-30 19:37:38 -0300

    fix.test(integration/Task/DeleteTask): login before try deleting tasks
    - correct error messages

--- back-end/__tests__/integration/Task/DeleteTask.spec.ts
+++ back-end/__tests__/integration/Task/DeleteTask.spec.ts
@@ -1,56 +1,69 @@
-import request from 'supertest';
-import shelljs from 'shelljs';
+import request from "supertest";
+import shelljs from "shelljs";
 
-import { expect } from 'chai';
+import { expect } from "chai";
 
-import { app } from '../../../src/app';
+import { app } from "../../../src/app";
 
-import { DATABASE_RESET, ENDPOINTS } from '../../utils/constants';
-import { tasks, users } from '../../utils/mocks';
+import { DATABASE_RESET, ENDPOINTS } from "../../utils/constants";
+import { tasks, users } from "../../utils/mocks";
 
 const [{ email, password }] = users;
 const [{ id }] = tasks;
 
-describe('Test endpoint DELETE /tasks/:id', () => {
+describe.only("Test endpoint DELETE /tasks/:id", () => {
   beforeEach(() => {
     shelljs.exec(DATABASE_RESET, { silent: true });
   });
 
-  it('Successfully delete a task', async () => {
-    const response = await request(app)
-      .delete(`${ENDPOINTS.TASK}/${id}`)
-      .auth(email, password);
+  it("Successfully delete a task", async () => {
+    await request(app)
+      .post(ENDPOINTS.LOGIN)
+      .send({ email, password })
+      .then(async (res) => {
+        const { token } = res.body;
 
-    expect(response.status).to.be.equal(200);
+        const response = await request(app)
+          .delete(`${ENDPOINTS.TASK}/${id}`)
+          .set("Authorization", token);
 
-    expect(response.body).to.be.undefined;
+        expect(response.status).to.be.equal(204);
+
+        expect(response.body).to.be.empty;
+      });
   });
 
-  it('Fail to delete a task without token', async () => {
+  it("Fail to delete a task without token", async () => {
     const response = await request(app).delete(`${ENDPOINTS.TASK}/${id}`);
 
     expect(response.status).to.be.equal(401);
-    expect(response.body).to.have.property('message');
-    expect(response.body.message).to.be.equal('Unauthorized');
+    expect(response.body).to.have.property("message");
+    expect(response.body.message).to.be.equal("Unauthorized");
   });
 
-  it('Fail to delete a task with invalid token', async () => {
+  it("Fail to delete a task with invalid token", async () => {
     const response = await request(app)
       .delete(`${ENDPOINTS.TASK}/${id}`)
-      .auth(email, 'invalid');
+      .set("Authorization", "invalid token");
 
     expect(response.status).to.be.equal(401);
-    expect(response.body).to.have.property('message');
-    expect(response.body.message).to.be.equal('Invalid token');
+    expect(response.body).to.have.property("message");
+    expect(response.body.message).to.be.equal("Expired or invalid token");
   });
 
-  it('Fail to delete a task with invalid id', async () => {
-    const response = await request(app)
-      .delete(`${ENDPOINTS.TASK}/invalid`)
-      .auth(email, password);
+  it("Fail to delete a task with invalid id", async () => {
+    await request(app)
+      .post(ENDPOINTS.LOGIN)
+      .send({ email, password })
+      .then(async (res) => {
+        const { token } = res.body;
+        const response = await request(app)
+          .delete(`${ENDPOINTS.TASK}/invalid`)
+          .set("Authorization", token);
 
-    expect(response.status).to.be.equal(404);
-    expect(response.body).to.have.property('message');
-    expect(response.body.message).to.be.equal('Task not found');
+        expect(response.status).to.be.equal(404);
+        expect(response.body).to.have.property("message");
+        expect(response.body.message).to.be.equal("Task does not exist");
+      });
   });
 });
