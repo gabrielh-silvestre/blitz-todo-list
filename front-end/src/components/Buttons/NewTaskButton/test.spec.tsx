2ba67f1c30d3cebe851d4ed5e3a3d05f9c862b4a
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-01 22:50:54 -0300

    feat.test(components/Buttons): cover NewTaskButton

---
+++ front-end/src/components/Buttons/NewTaskButton/test.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+
+import { NewTaskButton } from ".";
+
+describe("Test Component: NewTaskButton", () => {
+  it("01. should be a button", () => {
+    render(<NewTaskButton />);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeDefined();
+  });
+
+  it("02. should have a icon", () => {
+    render(<NewTaskButton />);
+
+    const icon = screen.getByTestId("add-task-icon");
+    expect(icon).toBeDefined();
+  });
+});
