a3e13b3db53e1e1917066e8ece96522b4ec184d8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-01 22:54:30 -0300

    feat.test(components/Buttons): cover DeleteTaskButton

---
+++ front-end/src/components/Buttons/DeleteTaskButton/test.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+
+import { DeleteTaskButton } from ".";
+
+describe("Test Component: DeleteTaskButton", () => {
+  it("01. should be a button", () => {
+    render(<DeleteTaskButton />);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeDefined();
+  });
+
+  it("02. should have a icon", () => {
+    render(<DeleteTaskButton />);
+
+    const icon = screen.getByTestId("delete-task-icon");
+    expect(icon).toBeDefined();
+  });
+});
