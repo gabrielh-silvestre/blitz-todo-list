91ae6a586add2b127b270b5cce1806fb89943c69
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-01 23:22:49 -0300

    feat.test(Buttons/DarkModeButton): add test case
    - verify if icon changes depending on mode

--- front-end/src/components/Buttons/DarkModeButton/test.spec.tsx
+++ front-end/src/components/Buttons/DarkModeButton/test.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { DarkModeButton } from ".";
 
@@ -10,10 +11,34 @@ describe("Test Component: DarkModeButton", () => {
     expect(button).toBeInTheDocument();
   });
 
-  it("02. should have a icon", () => {
+  it("02. should have with light mode icon", () => {
     render(<DarkModeButton />);
 
     const icon = screen.getByTestId("light-mode-icon");
     expect(icon).toBeInTheDocument();
+  });
+
+  it("03. should have with dark mode icon", () => {
+    render(<DarkModeButton />);
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    const icon = screen.getByTestId("dark-mode-icon");
+    expect(icon).toBeInTheDocument();
+  });
+
+  it("04. should toggle with light mode and dark mode", () => {
+    render(<DarkModeButton />);
+
+    const button = screen.getByRole("button");
+
+    const lightIcon = screen.getByTestId("light-mode-icon");
+    expect(lightIcon).toBeVisible();
+
+    userEvent.click(button);
+
+    const darkIcon = screen.getByTestId("dark-mode-icon");
+    expect(darkIcon).toBeVisible();
   });
 });
