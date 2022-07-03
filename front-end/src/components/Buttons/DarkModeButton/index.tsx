9d2adec854868bc59e07606216122a9fbeb56b91
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-03 15:32:19 -0300

    feat(Buttons/DarkModeButton): add internal state to control dark mode

--- front-end/src/components/Buttons/DarkModeButton/index.tsx
+++ front-end/src/components/Buttons/DarkModeButton/index.tsx
@@ -1,9 +1,20 @@
-import { MdOutlineDarkMode } from "react-icons/md";
+import { useState } from "react";
+import { MdOutlineDarkMode, MdDarkMode } from "react-icons/md";
 
 export function DarkModeButton() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <button>
-      <MdOutlineDarkMode data-testid="light-mode-icon" />
+    <button
+      onClick={() => {
+        setDarkMode(!darkMode);
+      }}
+    >
+      {darkMode ? (
+        <MdDarkMode data-testid="dark-mode-icon" />
+      ) : (
+        <MdOutlineDarkMode data-testid="light-mode-icon" />
+      )}
     </button>
   );
 }
