b11d82ba13551bd8030e1cc7be0549967b8e8d2e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-03 19:32:49 -0300

    feat(components/Headers): create MainHeader
    - render site title, dark mode button and user button
    - apply basic style
    - without functionallity

---
+++ front-end/src/components/Headers/MainHeader/styles.ts
@@ -0,0 +1,22 @@
+import tw from "tailwind-styled-components";
+
+export const ContentContainer = tw.div`
+  w-full
+
+  flex
+  justify-between
+  items-center
+
+  text-3xl
+`;
+
+export const Title = tw.h1`
+  font-semibold
+`;
+
+export const IconsContainer = tw.div`
+  w-20
+
+  flex
+  justify-between
+`;
