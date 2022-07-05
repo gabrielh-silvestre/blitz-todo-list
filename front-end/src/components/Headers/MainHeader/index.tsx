3b9706568bec83b9a11893b6631093506a421d0d
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-04 23:33:56 -0300

    feat(Headers/MainHeader): add SignIn/SignUp forms on user icon

--- front-end/src/components/Headers/MainHeader/index.tsx
+++ front-end/src/components/Headers/MainHeader/index.tsx
@@ -1,10 +1,32 @@
+import Modal from "react-modal";
 import { HiUserCircle } from "react-icons/hi";
 
 import { DarkModeButton } from "../../Buttons/DarkModeButton";
+import { SignInForm } from "../../Forms/SignInForm";
+import { SignUpForm } from "../../Forms/SignUpForm";
 
 import { ContentContainer, IconsContainer, Title } from "./styles";
+import { useEffect, useState } from "react";
+import { userStore } from "../../../stores/user";
 
 export function MainHeader() {
+  const { userToken } = userStore((state) => state);
+
+  const [hasAccount, setHasAccount] = useState(true);
+  const [isSignInFormOpen, setIsSignInFormOpen] = useState(false);
+
+  const handleOpenSignInForm = () => {
+    setIsSignInFormOpen(true);
+  };
+
+  const handleCloseSignInForm = () => {
+    setIsSignInFormOpen(false);
+  };
+
+  useEffect(() => {
+    console.log({ userToken });
+  }, [isSignInFormOpen]);
+
   return (
     <>
       <ContentContainer>
@@ -12,11 +34,30 @@ export function MainHeader() {
 
         <IconsContainer>
           <DarkModeButton />
-          <button>
+          <button onClick={handleOpenSignInForm}>
             <HiUserCircle />
           </button>
         </IconsContainer>
       </ContentContainer>
+
+      <Modal
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+        isOpen={isSignInFormOpen}
+        onRequestClose={handleCloseSignInForm}
+      >
+        {hasAccount ? (
+          <SignInForm
+            closeForm={() => setIsSignInFormOpen(false)}
+            alreadyHaveAccount={() => setHasAccount(false)}
+          />
+        ) : (
+          <SignUpForm
+            closeForm={() => setIsSignInFormOpen(false)}
+            alreadyHaveAccount={() => setHasAccount(true)}
+          />
+        )}
+      </Modal>
     </>
   );
 }
