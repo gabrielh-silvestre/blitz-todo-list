8c58c9eba56314c019aa8e99f7d88a8a7b0e71a7
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-04 23:34:52 -0300

    feat: remove hard-coded token
    - create style for form modals

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,9 @@
+import Modal from "react-modal";
 import { Toaster } from "react-hot-toast";
 
 import { SideTasks } from "./components/Aside/SideTasks";
 import { MainHeader } from "./components/Headers/MainHeader";
 import { TaskDetail } from "./components/Main/TaskDetail";
-
-import { userStore } from "./stores/user";
 
 import {
   AsideContainer,
@@ -14,12 +13,11 @@ import {
   MainContainer,
 } from "./styles";
 
-const TOKEN =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiY2w0cTI4ZGdlMDAwMTNhODJ5aHQ1a2x4biJ9LCJpYXQiOjE2NTY5NzA1NDEsImV4cCI6MTY1NzIyOTc0MX0.88erZTM1xlSAOvaHS8IS3oB1hRq5y4kibLAIyudmW9k";
+import "./App.css";
+
+Modal.setAppElement("#root");
 
 function App() {
-  userStore.setState({ userToken: TOKEN });
-
   return (
     <Container>
       <HeaderContainer>
