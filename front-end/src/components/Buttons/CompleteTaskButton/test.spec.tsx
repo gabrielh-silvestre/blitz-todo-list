fed6134bd0dad2035207b4442326b317d0a68016
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-05 11:48:32 -0300

    hot.fix(components/Buttons): correct props or remove test files

--- front-end/src/components/Buttons/CompleteTaskButton/test.spec.tsx
+++ front-end/src/components/Buttons/CompleteTaskButton/test.spec.tsx
@@ -4,21 +4,21 @@ import { CompleteTaskButton } from ".";
 
 describe("Test Component: CompleteTaskButton", () => {
   it("01. should be a button", () => {
-    render(<CompleteTaskButton />);
+    render(<CompleteTaskButton taskId="1" />);
 
     const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
   });
 
   it("02. should have a icon", () => {
-    render(<CompleteTaskButton />);
+    render(<CompleteTaskButton taskId="1" />);
 
     const icon = screen.getByTestId("complete-task-icon");
     expect(icon).toBeInTheDocument();
   });
 
   it("03. when completed should not have a icon", () => {
-    render(<CompleteTaskButton completed />);
+    render(<CompleteTaskButton taskId="1" completed />);
 
     const icon = screen.queryByTestId("complete-task-icon");
     expect(icon).not.toBeVisible();
