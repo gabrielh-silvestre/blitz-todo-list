7e8d4420faa86369568197d93ab1fcb76b45438a
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-05 10:52:00 -0300

    feat(Buttons/CompleteTaskButton): add functionallity
    - complete task on click, and hide if task is already completed

--- front-end/src/components/Buttons/CompleteTaskButton/index.tsx
+++ front-end/src/components/Buttons/CompleteTaskButton/index.tsx
@@ -2,11 +2,20 @@ import { HiCheck } from "react-icons/hi";
 
 import type { CompleteTaskButtonProps } from "./propTypes";
 
+import { useCompleteTask } from "../../../stores/task/useCases/CompleteTask";
+
 export function CompleteTaskButton({
+  taskId,
   completed = false,
 }: CompleteTaskButtonProps) {
+  const { mutate } = useCompleteTask();
+
+  const handleCompleteTask = () => {
+    mutate(taskId);
+  };
+
   return (
-    <button hidden={completed}>
+    <button hidden={completed} onClick={handleCompleteTask}>
       <HiCheck data-testid="complete-task-icon" />
     </button>
   );
