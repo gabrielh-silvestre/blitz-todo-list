d563b0280b44b7ef87552e0a28d78dc500624c68
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-05 01:56:48 -0300

    feat(Items/TaskItem): get task details

--- front-end/src/components/Items/TaskItem/index.tsx
+++ front-end/src/components/Items/TaskItem/index.tsx
@@ -3,14 +3,22 @@ import { DeleteTaskButton } from "../../Buttons/DeleteTaskButton";
 
 import type { TaskItemProps } from "./propTypes";
 
+import { useGetTaskById } from "../../../stores/task/useCases/GetTaskById";
+
 import { ButtonContainer, ContentContainer, TaskTitle } from "./styles";
 
 export function TaskItem({ id, status, title }: TaskItemProps) {
+  const { mutate } = useGetTaskById(id);
+
+  const handleSelectTask = () => {
+    mutate(id);
+  };
+
   return (
     <ContentContainer>
       <span>{status}</span>
 
-      <TaskTitle>{title}</TaskTitle>
+      <TaskTitle onClick={handleSelectTask}>{title}</TaskTitle>
 
       <ButtonContainer>
         <CompleteTaskButton />
