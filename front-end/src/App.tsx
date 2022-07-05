da5e2b35957ebf208ccbbbb91e871dadabbe69b8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-04 21:52:16 -0300

    feat: implement ReactQuery provider and React Hot Toast
    - define user token manually, temporary solution

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,10 @@
+import { Toaster } from "react-hot-toast";
+
 import { SideTasks } from "./components/Aside/SideTasks";
 import { MainHeader } from "./components/Headers/MainHeader";
 import { TaskDetail } from "./components/Main/TaskDetail";
+
+import { userStore } from "./stores/user";
 
 import {
   AsideContainer,
@@ -10,7 +14,12 @@ import {
   MainContainer,
 } from "./styles";
 
+const TOKEN =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiY2w0cTI4ZGdlMDAwMTNhODJ5aHQ1a2x4biJ9LCJpYXQiOjE2NTY5NzA1NDEsImV4cCI6MTY1NzIyOTc0MX0.88erZTM1xlSAOvaHS8IS3oB1hRq5y4kibLAIyudmW9k";
+
 function App() {
+  userStore.setState({ userToken: TOKEN });
+
   return (
     <Container>
       <HeaderContainer>
@@ -19,13 +28,14 @@ function App() {
 
       <ContentContainer>
         <AsideContainer>
-          <SideTasks tasks={[]} />
+          <SideTasks />
         </AsideContainer>
 
         <MainContainer>
           <TaskDetail />
         </MainContainer>
       </ContentContainer>
+      <Toaster />
     </Container>
   );
 }
