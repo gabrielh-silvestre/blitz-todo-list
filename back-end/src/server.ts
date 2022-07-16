4e66b0f21d30978f0fe3b88cea89d0a0767d8d8f
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-15 21:45:39 -0300

    hotfix: correct backend port env var
    - remove .env.example from root
    - addapt docker compose

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { app } from "./app";
 
-const PORT = process.env.BACK_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
